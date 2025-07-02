eead945f0f33f9d7659d355d31a53b8935f1e8e2
Z3r0Glitch22 <131920670+Z3r0Glitch22@users.noreply.github.com>
2025-07-01 19:28:15 -0700

    Add files via upload

--- ios/ViewController.mm
+++ ios/ViewController.mm
@@ -1,79 +1,22 @@
+#import <MetalKit/MetalKit.h>
 #import "ViewController.h"
-#import <GameController/GameController.h>
+#import "engine.h"
 
-extern "C" void engine_run(const char* rom_path);
-extern "C" void engine_frame();
-extern "C" void engine_draw_frame(id<CAMetalDrawable> drawable, id<MTLTexture> framebuffer);
+@interface ViewController () <MTKViewDelegate>
+@end
 
 @implementation ViewController {
     MTKView *_mtkView;
-    CADisplayLink *_displayLink;
 }
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    self.view.backgroundColor = [UIColor blackColor];
-
-    _mtkView = [[MTKView alloc] initWithFrame:self.view.bounds];
-    _mtkView.device = MTLCreateSystemDefaultDevice();
-    _mtkView.delegate = self;
-    _mtkView.enableSetNeedsDisplay = NO;
-    _mtkView.paused = YES;
-    _mtkView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    [self.view addSubview:_mtkView];
-
-    [self presentROMPicker];
-    [self setupControllers];
+// Called when the drawable size of the view changes
+- (void)mtkView:(MTKView *)view drawableSizeWillChange:(CGSize)size {
+    // Optional: Handle resizing if needed
 }
 
-- (void)presentROMPicker {
-    UIDocumentPickerViewController *picker =
-        [[UIDocumentPickerViewController alloc] initForOpeningContentTypes:@[[UTType data]] asCopy:YES];
-    picker.delegate = self;
-    picker.modalPresentationStyle = UIModalPresentationFullScreen;
-    [self presentViewController:picker animated:YES completion:nil];
-}
-
-- (void)documentPicker:(UIDocumentPickerViewController *)controller didPickDocumentsAtURLs:(NSArray<NSURL *> *)urls {
-    NSURL *romURL = urls.firstObject;
-    if (romURL) {
-        NSString *romPath = [romURL path];
-        NSLog(@"ROM Selected: %@", romPath);
-        engine_run([romPath UTF8String]);
-        [self startGameLoop];
-    }
-}
-
-- (void)startGameLoop {
-    _displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(step)];
-    [_displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
-}
-
-- (void)step {
-    engine_frame();
-    [_mtkView draw];
-}
-
+// Called every frame to render content
 - (void)drawInMTKView:(MTKView *)view {
-    id<CAMetalDrawable> drawable = view.currentDrawable;
-    if (!drawable) return;
-    id<MTLTexture> framebuffer = drawable.texture;
-    engine_draw_frame(drawable, framebuffer);
+    engine_draw_frame(); // Ensure this function is defined in your engine
 }
 
-- (void)setupControllers {
-    [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(controllerConnected:)
-                                             name:GCControllerDidConnectNotification object:nil];
-    [GCController startWirelessControllerDiscoveryWithCompletionHandler:nil];
-}
-
-- (void)controllerConnected:(NSNotification *)note {
-    GCController *controller = note.object;
-    GCExtendedGamepad *gamepad = controller.extendedGamepad;
-    if (gamepad) {
-        gamepad.valueChangedHandler = ^(GCExtendedGamepad *pad, GCControllerElement *element) {
-            // TODO: Forward controller input to engine here
-        };
-    }
-}
 @end
